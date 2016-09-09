9a49141906715e4219b961ac0cc9d383315bf67e
devsheder <adrien.lasselle@gmail.com>
2016-09-09 14:49:27 +0200

    Exposition BT d'un service via Bleno ok. RAF : créer les caractéristiques du service

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-var Bleno = require("./node_modules/bleno/lib/bleno")
+var bleno = require("bleno");
+var BlenoPrimaryService = bleno.PrimaryService;
 
-module.exports = new Bleno();
+var uuidHWService = "hw00";
+
+bleno.on('stateChange', function(state) {
+	console.log(state);
+	if (state === 'poweredOn') {
+		bleno.startAdvertising("helloworld", [uuidHWService]);
+	} else {
+		bleno.stopAdvertising();
+	}
+});
+
+bleno.on('advertisingStart', function(error) {
+	console.log(error ? "error" + error : "success");
+	
+	if (!error) {
+		bleno.setServices([
+			new BlenoPrimaryService({
+				uuid:uuidHWService
+			})
+		]);
+	}
+});
