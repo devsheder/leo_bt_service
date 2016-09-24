fcebf91e02e908981c8d8364d72f573781cca978
devsheder <adrien.lasselle@gmail.com>
2016-09-24 22:38:20 +0200

    Ajout de la roue gauche

--- stop.js
+++ stop.js
@@ -7,44 +7,20 @@ Cylon.robot({
   },
 
   devices: {
-    led: { driver: 'led', pin: 11}
+	led1: { driver: 'led', pin: 11},
+	led2: { driver: 'led', pin: 13},
+	led3: { driver: 'led', pin: 15},
+	led4: { driver: 'led', pin: 12},
+	led5: { driver: 'led', pin: 16},
+	led6: { driver: 'led', pin: 18}
   },
 
   work: function(my) {
-    my.led.turnOff(function(err,val){
-        console.log(val);
-    });
-  }
-}).start();
-
-Cylon.robot({
-  connections: {
-    raspi: { adaptor: 'raspi' }
-  },
-
-  devices: {
-    led: { driver: 'led', pin: 13}
-  },
-
-  work: function(my) {
-    my.led.turnOff(function(err,val){
-        console.log(val);
-    });
-  }
-}).start();
-
-Cylon.robot({
-  connections: {
-    raspi: { adaptor: 'raspi' }
-  },
-
-  devices: {
-    led: { driver: 'led', pin: 15}
-  },
-
-  work: function(my) {
-    my.led.turnOff(function(err,val){
-        console.log(val);
-    });
+    my.led1.turnOff();
+    my.led4.turnOff();
+    my.led2.turnOn();
+    my.led3.turnOn();
+    my.led5.turnOn();
+    my.led6.turnOn();
   }
 }).start();
